5bca822687955ce3996042189e4844ae06bab903
“[Satish <[satishyp24@gmail.com]>
2025-07-06 00:05:34 -0400

    deleted:    terraform/deploy.bat

--- terraform/deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing Lambda dependencies...
-cd ..\lambda
-pip install -r requirements.txt -t .
-cd ..\terraform
-echo Running Terraform...
-terraform init
-terraform plan
-terraform apply
